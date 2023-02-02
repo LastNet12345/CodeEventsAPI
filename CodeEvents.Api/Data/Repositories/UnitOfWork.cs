2e690c9b2907160b2376435506f1a2e737b8354a
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-02-02 14:23:01 +0100

    Add UnitOfWork

---
+++ CodeEvents.Api/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeEvents.Api.Data.Repositories
+{
+    public class UnitOfWork
+    {
+        private readonly CodeEventsApiContext db;
+
+        public CodeEventRepository CodeEventRepository { get; }
+
+        public UnitOfWork(CodeEventsApiContext db)
+        {
+            this.db = db;
+            CodeEventRepository = new CodeEventRepository(db);
+        }
+
+        public async Task CompleteAsync()
+        {
+            await db.SaveChangesAsync();
+        }
+    }
+}
