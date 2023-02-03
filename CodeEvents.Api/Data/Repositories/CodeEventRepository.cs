523a8584c5ba3e7d9c06e7a2d930a987c434c22d
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-02-03 12:01:11 +0100

    IncludeLectures - Not Working!

--- CodeEvents.Api/Data/Repositories/CodeEventRepository.cs
+++ CodeEvents.Api/Data/Repositories/CodeEventRepository.cs
@@ -12,9 +12,13 @@ namespace CodeEvents.Api.Data.Repositories
             this.db = db;
         }
 
-        public async Task<IEnumerable<CodeEvent>> GetAsync()
+        public async Task<IEnumerable<CodeEvent>> GetAsync(bool includeLectures)
         {
-            return await db.CodeEvent.ToListAsync();
+            return includeLectures ? await db.CodeEvent.Include(c => c.Location)
+                                                       .Include(c => c.Lectures)
+                                                       .ToListAsync() :
+                                     await db.CodeEvent.Include(c => c.Location)
+                                                       .ToListAsync();
         }
     }
 }
