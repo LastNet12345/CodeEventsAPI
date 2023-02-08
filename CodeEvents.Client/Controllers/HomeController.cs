6091691f7a17d89ff65287cef857961f6a498304
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-02-08 14:28:27 +0100

    GetWithRequestMessage

--- CodeEvents.Client/Controllers/HomeController.cs
+++ CodeEvents.Client/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using CodeEvents.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace CodeEvents.Client.Controllers
@@ -10,26 +11,43 @@ namespace CodeEvents.Client.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly HttpClient httpClient;
+        private const string json = "application/json";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
             httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("https://localhost:7181");
+           // httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
         }
 
         public async Task<IActionResult> Index()
         {
 
-            var res = await SimpleGet();
+            //var res = await SimpleGet();
+            var res = await GetWithRequestMessage();
 
 
             return View();
         }
 
+        private async Task<IEnumerable<CodeEventDto?>> GetWithRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/events");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+            var codeEvents = JsonSerializer.Deserialize<IEnumerable<CodeEventDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return codeEvents!;
+        }
+
         private async Task<IEnumerable<CodeEventDto?>> SimpleGet()
         {
-            var response = await httpClient.GetAsync("api/events/?includeLectures=true");
+            var response = await httpClient.GetAsync("api/events/?includelectures=true");
             response.EnsureSuccessStatusCode();
 
             var result = await response.Content.ReadAsStringAsync();
