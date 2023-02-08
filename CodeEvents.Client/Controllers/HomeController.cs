e42d3f1f995ae4acaf394363142eb9b742605a74
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-02-08 14:09:57 +0100

    SimpleGet

--- CodeEvents.Client/Controllers/HomeController.cs
+++ CodeEvents.Client/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
-﻿using CodeEvents.Client.Models;
+﻿using CodeEvents.Api.Core.DTOs;
+using CodeEvents.Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace CodeEvents.Client.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient httpClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:7181");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+
+            var res = await SimpleGet();
+
+
             return View();
+        }
+
+        private async Task<IEnumerable<CodeEventDto?>> SimpleGet()
+        {
+            var response = await httpClient.GetAsync("api/events/?includeLectures=true");
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var codeEvents = JsonSerializer.Deserialize<IEnumerable<CodeEventDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return codeEvents!;
         }
 
         public IActionResult Privacy()
