5d7ce89ff073866c3e95b57def98463b9838b628
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-02-09 13:12:18 +0100

    Move service registration

--- CodeEvents.Client/Program.cs
+++ CodeEvents.Client/Program.cs
@@ -11,16 +11,6 @@ namespace CodeEvents.Client
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             //1.
             builder.Services.AddHttpClient();
 
@@ -36,6 +26,16 @@ namespace CodeEvents.Client
                 client.BaseAddress = new Uri("");
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
